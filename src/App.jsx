a75d7e6fa3d1523e1dc73431d103b15add63b7d0
shivabollam <shivabollam07@gmail.com>
2024-08-31 12:35:16 +0530

    commit-1

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 import './App.css'
 import Products from './Components/Product'
+import image1 from './assets/fresh-milk.png'
+import image2 from './assets/cottage-cheese.png'
+import image3 from './assets/brocoli.png'
+import image4 from './assets/oranges.png'
+import image5 from './assets/olive-oil.png'
+
 
 let products = [
   {
@@ -7,7 +13,7 @@ let products = [
     pName: 'Fresh Milk',
     desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit fuga autem maiores necessitatibus.',
     isAvailable: true,
-    image: 'assets/fresh-milk.png',
+    image: image1,
     price: 12
   },
   {
@@ -15,7 +21,7 @@ let products = [
     pName: 'Cottage Cheese',
     desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit fuga autem maiores necessitatibus.',
     isAvailable: false,
-    image: "assets/cottage-cheese.png",
+    image: image2,
     price: 10
   },
   {
@@ -23,7 +29,7 @@ let products = [
     pName: 'Brocoli',
     desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit fuga autem maiores necessitatibus.',
     isAvailable: true,
-    image: "assets/brocoli.png",
+    image: image3,
     price: 15
   },
   {
@@ -31,7 +37,7 @@ let products = [
     pName: 'oranges',
     desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit fuga autem maiores necessitatibus.',
     isAvailable: true,
-    image: "assets/oranges.png",
+    image: image4,
     price: 20
   },
   {
@@ -39,7 +45,7 @@ let products = [
     pName: 'Olive oil',
     desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit fuga autem maiores necessitatibus.',
     isAvailable: false,
-    image: "assets/olive-oil.png",
+    image: image5,
     price: 14
   }
 ]
@@ -49,12 +55,48 @@ function App() {
   return (
     <div className='row'>
       <div className='col-lg-8 mx-auto'>
-        <Products ></Products>
-        <Products ></Products>
-        <Products ></Products>
-        <Products ></Products>
-        <Products ></Products>
-        <Products ></Products>
+        <Products
+          id = {products[0].pID}
+          name = {products[0].pName}
+          desc = {products[0].desc}
+          isAvailable = {products[0].isAvailable}
+          image = {products[0].image}
+          price = {products[0].price}
+         ></Products>
+        <Products
+          id = {products[1].pID}
+          name = {products[1].pName}
+          desc = {products[1].desc}
+          isAvailable = {products[1].isAvailable}
+          image = {products[1].image}
+          price = {products[1].price}
+          ></Products>
+        <Products
+          id = {products[2].pID}
+          name = {products[2].pName}
+          desc = {products[2].desc}
+          isAvailable = {products[2].isAvailable}
+          image = {products[2].image}
+          price = {products[2].price}
+          ></Products>
+        <Products
+          id = {products[3].pID}
+          name = {products[3].pName}
+          desc = {products[3].desc}
+          isAvailable = {products[3].isAvailable}
+          image = {products[3].image}
+          price = {products[3].price}
+          ></Products>
+        <Products
+          id = {products[4].pID}
+          name = {products[4].pName}
+          desc = {products[4].desc}
+          isAvailable = {products[4].isAvailable}
+          image = {products[4].image}
+          price = {products[4].price}
+          ></
+        Products>
+       
       </div>
     </div>
   )
--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -1,40 +1,65 @@
-import image from '../assets/image.png';
+import React, { useState } from 'react';
 
-let productCount = -2;
-let isAvailable = 'Available';
-let bgClass = 'badge-margin-left-240 badge';
-bgClass += isAvailable === 'Available' ? ' bg-success' : ' bg-danger';
+function Products(props) {
+  // Manage product count state within the component
+  const [productCount, setProductCount] = useState(0);
 
-let style = {
-  'padding': '0px 20px',
-  'fontSize': 20
-}
+  // Update the badge class dynamically based on availability
+  let bgClass = 'badge-margin-left-240 badge'
+  if (props.isAvailable) {
+    bgClass += ' bg-success';
+  }
+  else {
+    bgClass += ' bg-danger';
+  }
 
-function displayFormatedProductCount() {
-  const x = <p>zero</p>
-  return productCount > 0 ? productCount : x;
-}
+  const style = {
+    padding: '0px 20px',
+    fontSize: 20,
+  };
 
-function Products() {
+  // Format the product count for display
+  function displayFormatedProductCount() {
+    return productCount > 0 ? productCount : <p>zero</p>;
+  }
+
   return (
     <ul className="list-group shadow">
       <li className="list-group-item">
         <div className="media align-items-lg-center flex-column flex-lg-row p-3 d-flex">
           <div className="media-body order-2 order-lg-1">
-            <h5 className="mt-0 font-weight-bold mb-2">Fresh Milk</h5>
-            <p className="font-italic text-muted mb-0 small">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit fuga autem maiores necessitatibus.</p>
+            <h5 className="mt-0 font-weight-bold mb-2">{props.name}</h5>
+            <p className="font-italic text-muted mb-0 small">{props.desc}</p>
             <div className="d-flex align-items-center justify-content-start mt-1">
-              <h6 className="font-weight-bold my-2">$120.00</h6>
-              <button className="btn btn-primary">-</button>
-              <span style={style} >{displayFormatedProductCount()}</span>
-              <button className="btn btn-primary">+</button>
-              <span className={bgClass}>{isAvailable}</span>
+              <h6 className="font-weight-bold my-2">${props.price}</h6>
+              <span className="text-muted font-weight-normal font-italic">
+              </span>
+              <button
+                className="btn btn-primary"
+                onClick={() => setProductCount((count) => Math.max(count - 1, 0))}
+              >
+                -
+              </button>
+              <span style={style}>{displayFormatedProductCount()}</span>
+              <button
+                className="btn btn-primary"
+                onClick={() => setProductCount((count) => count + 1)}
+              >
+                +
+              </button>
+              <span className={bgClass}>{props.isAvailable ? 'Available' : 'UAvailable'}</span>
             </div>
-          </div><img src={image} alt="Generic placeholder image" width="200" className="ml-lg-5 order-1 order-lg-2" />
+          </div>
+          <img
+            src={props.image}
+            alt={`${props.name} image`}
+            width="200"
+            className="ml-lg-5 order-1 order-lg-2"
+          />
         </div>
       </li>
     </ul>
-  )
+  );
 }
 
 export default Products;
