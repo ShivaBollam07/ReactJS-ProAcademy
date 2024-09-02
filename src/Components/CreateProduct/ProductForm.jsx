3142a982a34d27e5b5de0e76121925c857f8d4f0
shivabollam <shivabollam07@gmail.com>
2024-09-02 09:05:17 +0530

    updated ProductForm

--- src/Components/CreateProduct/ProductForm.jsx
+++ src/Components/CreateProduct/ProductForm.jsx
@@ -1,88 +1,110 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 
-function ProductForm(){
-    let [productName, setProductName] = useState('');
-    let [productPrice, setProductPrice] = useState('');
-    let [productDescription, setProductDescription] = useState('');
-    let [productAvailability, setProductAvailability] = useState('');
-    let [productImage, setProductImage] = useState('');
+function ProductForm() {
+    // let [productName, setProductName] = useState('');
+    // let [productPrice, setProductPrice] = useState('');
+    // let [productDescription, setProductDescription] = useState('');
+    // let [productAvailability, setProductAvailability] = useState('');
+    // let [productImageURL, setProductImageURL] = useState('');
+    let [userInput, setUserInput] = useState({
+        productName: '',
+        productPrice: '',
+        productDescription: '',
+        productAvailability: '',
+        productImageURL: ''
+    })
 
 
     function nameInputHandler(event) {
-        setProductName(event.target.value);
+        setUserInput({
+            ...userInput,
+            productName: event.target.value
+        })
     }
 
     function ProductPriceHandler(event) {
-        setProductPrice(event.target.value);
+        setUserInput({
+            ...userInput,
+            productPrice: event.target.value
+        })
     }
 
     function ProductdescriptionHandler(event) {
-        setProductDescription(event.target.value);
+        setUserInput({
+            ...userInput,
+            productDescription: event.target.value
+        })
     }
 
     function ProductAvailabilityHandler(event) {
-        setProductAvailability(event.target.checked);
+        setUserInput({
+            ...userInput,
+            productAvailability: event.target.value
+        })
     }
 
     function FileInputHandler(event) {
-        setProductImage(event.target.value);
+        setUserInput({
+            ...userInput,
+            productImageURL: event.target.value
+        })
     }
 
-    
 
 
 
-    return(
+
+    return (
         <div className="row g-3">
-        <div className="col-md-6">
-            <label for="name">Product Name</label>
-            <input type="text" 
-                    className="form-control" 
-                    id="name" 
-                    placeholder="Product Name" 
+            <div className="col-md-6">
+                <label for="name">Product Name</label>
+                <input type="text"
+                    className="form-control"
+                    id="name"
+                    placeholder="Product Name"
                     onChange={nameInputHandler}
-                    />
-        </div>
-        <div className="col-md-6">
-            <label for="price">Product Price</label>
-            <input type="number" 
-                    min="0.01" step="0.01" 
-                    className="form-control" 
-                    id="price" 
+                />
+            </div>
+            <div className="col-md-6">
+                <label for="price">Product Price</label>
+                <input type="number"
+                    min="0.01" step="0.01"
+                    className="form-control"
+                    id="price"
                     onChange={ProductPriceHandler}
 
                     placeholder="Product Price" />
-        </div>
+            </div>
 
-        <div className="form-group">
-            <label for="description">Product Description</label>
-            <input type="text" 
-                    className="form-control" 
-                    id="description" 
+            <div className="form-group">
+                <label for="description">Product Description</label>
+                <input type="text"
+                    className="form-control"
+                    id="description"
                     onChange={ProductdescriptionHandler}
 
                     placeholder="Product Description" />
+            </div>
+
+            <div className="form-check form-switch">
+                <input className="form-check-input" type="checkbox"
+                    onChange={ProductAvailabilityHandler}
+
+                    role="switch" id="isAvailable" />
+                <label className="form-check-label" for="isAvailable">Is product available in stock?</label>
+            </div>
+
+            <div className="form-group">
+                <label for="select-image">Select product image</label>
+                <input type="file" className="form-control" id="select-image"
+                    onChange={FileInputHandler}
+
+                />
+            </div>
+
+
+            <button type="submit" className="btn btn-primary">Add Product</button>
         </div>
-
-        <div className="form-check form-switch">
-            <input className="form-check-input" type="checkbox" 
-            onChange={ProductAvailabilityHandler}
-
-            role="switch" id="isAvailable" />
-            <label className="form-check-label" for="isAvailable">Is product available in stock?</label>
-        </div>
-
-        <div className="form-group">
-            <label for="select-image">Select product image</label>
-            <input type="file" className="form-control" id="select-image"
-            onChange={FileInputHandler}
-
-             />
-        </div>
-        
-
-        <button type="submit" className="btn btn-primary">Add Product</button>
-    </div>
     )
 }
 
